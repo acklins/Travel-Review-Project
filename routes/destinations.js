7eb84846dc8e8b8da4bf114fd786b83a60104bd7
cgrobbin <cgrobbin@gmail.com>
2021-02-23 17:00:53 -0600

    Writes route for search results page, connects to ejs file

--- routes/destinations.js
+++ routes/destinations.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios')
 const db = require("../models")
 
 // Routes
 
 // GET /destinations - After search, renders a page of vacation destinations to pick from
 router.get('/', (req, res) => {
-    res.render()
+    // let place = req.query
+    let apiUrl = 'https://api.teleport.org/api/cities/?search=rome'
+    axios.get(apiUrl)
+    .then((responseData) => {
+        let data = responseData.data._embedded['city:search-results'][0]
+        let geoid = data._links['city:item'].href
+        axios.get(geoid)
+        .then((responseData) => {
+            let specData = responseData.data
+            console.log(specData)
+            res.render('destinations/search.ejs', {specData: specData})
+        })
+    })
+    // res.send('Found Data')
 })
 
 // GET /destinations/:id - Render a page with a single destination with info about it
