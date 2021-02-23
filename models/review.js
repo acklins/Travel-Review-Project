a9b4a4cb1014dd9229f6c608965dd7c12a8ccb79
cgrobbin <cgrobbin@gmail.com>
2021-02-23 12:24:06 -0600

    Adds functionality to associations, removes unnecessary model/migration

--- models/review.js
+++ models/review.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
-      models.review.belongsToMany(models.destination, {through: destinationsReviews})
+      models.review.belongsTo(models.destination)
       models.review.belongsTo(models.user)
     }
   };
