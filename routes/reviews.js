092554a9cf455136cdccdcc9033a0bc7ac5925b2
Della <dmcmillan100@gmnail.com>
2021-02-25 11:46:02 -0800

    route mapping ofr put & delete

--- routes/reviews.js
+++ routes/reviews.js
@@ -7,4 +7,13 @@ const db = require("../models")
 // GET /destinations/:id/reviews - Renders a page of reviews about a destination
 router.get('')
 
+//PUT /destinations/:id/reviews
+router.put('/:id/reviews/:id/edit', (req, res) =>{
+    res.redirect("/:id/reviews")
+})
+router.delete("/:id/reviews/:id", (req, res) => {
+    res.redirect("/:id/reviews")
+})
+
+
 module.exports = router;
