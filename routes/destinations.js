8faa2541b42596a4e0f9fdac003da1281f3d275f
cgrobbin <cgrobbin@gmail.com>
2021-02-26 16:36:49 -0600

    Finalizes all routes, full CRUD functionality

--- routes/destinations.js
+++ routes/destinations.js
@@ -5,6 +5,8 @@ const db = require("../models")
 const isLoggedIn = require('../middleware/isLoggedIn')
 const methodOverride = require('method-override')
 
+router.use(methodOverride('_method'))
+
 // Routes
 
 // GET /destinations/:destid - Render a page with a single destination with info about it
@@ -52,20 +54,41 @@ router.get('/:destid/reviews/new', isLoggedIn, (req, res) => {
 })
 
 // GET /destinations/:destid/reviews/:revid/edit - Renders review form with relevant info for editing
-router.get('/:destid/reviews/:revid/edit', (req, res) => {
-    res.render('reviews/editreview.ejs')
+router.get('/:destid/reviews/:revid/edit', isLoggedIn, (req, res) => {
+    db.review.findOne({
+        where: {id: req.params.revid},
+        include: [db.user, db.destination]
+    }) .then((review) => {
+        const revData = {
+            destinationName: review.destination.dataValues.city,
+            destinationId: review.destination.dataValues.id,
+            userName: review.user.dataValues.name,
+            reviewText: review.content,
+            reviewId: review.id
+        }
+        res.render('reviews/editreview.ejs', {revData})
+    })
 })
 
 // POST /destinations/:destid/reviews/new - Posts new review to database and to destination/:id/reviews
-router.post('/:destid/reviews/new', (req, res) => {
-    res.redirect('/:destid/reviews')
+router.post('/:destid/reviews/new', isLoggedIn, (req, res) => {
+    db.review.create({
+        userId: req.user.id,
+        destinationId: req.params.destid,
+        content: req.body.reviewcontent
+    }).then(() => {
+        res.redirect(`/destinations/${req.params.destid}/reviews`)
+    })
 })
 
 // PUT /destinations/:destid/reviews/:revid/edit - Updates review info on page and in database
 router.put('/:destid/reviews/:revid/edit', isLoggedIn, (req, res) => {
-    db.review.update({content:req.body},
-        {where:{id:revid }}
-    ).then(res.redirect('/:destid/reviews'))
+    db.review.update({content:req.body.reviewcontent},
+        {where:{id: req.params.revid }}
+        ).then((review) => {
+            console.log(review)
+            res.redirect(`/destinations/${req.params.destid}/reviews`)
+    })
 })
 
 // DELETE /destinaitons/:destid/reviews/:revid - Deletes a review from the page and database
@@ -73,7 +96,7 @@ router.delete('/:destid/reviews/:revid', isLoggedIn, (req, res) => {
     db.review.destroy({
         where: {id: req.params.revid}
     }).then( 
-    res.redirect('/:destid/reviews') )
+    res.redirect(`/destinations/${req.params.destid}/reviews`) )
 })
 
 
